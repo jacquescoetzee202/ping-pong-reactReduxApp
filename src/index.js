9469bbf341ba5f027c912776b20834ace122d002
Jacques <jacquescoetzee91@gmail.com>
2020-12-07 13:09:21 +0000

    project set up to include redux store in index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from "redux";
+
+const initial = {
+  player1: 0,
+  player2: 0,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PLAYER1": return { ...state, player1: state.player1 + 1};
+    case "PLAYER2": return { ...state, player2: state.player2 + 1};
+    default : return state;
+  }
+};
+
+const store = createStore(reducer, initial);
+
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render); // render on state change
+render(); // get the page to render first time before any state changes
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
