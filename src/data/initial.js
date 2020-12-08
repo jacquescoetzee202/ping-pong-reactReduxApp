2a49d98dadc355b87e9a7f8145dbe19117b48a4b
Jacques <jacquescoetzee91@gmail.com>
2020-12-08 10:07:15 +0000

    created data dir and sperated initial state and reducer funcntions

---
+++ src/data/initial.js
@@ -0,0 +1,9 @@
+const initial = {
+    player1: 0,
+    player2: 0,
+    servingP1: true,
+    winner: 0,
+    gameHistory: [],
+}
+
+export default initial;
---
+++ src/data/reducer.js
@@ -0,0 +1,69 @@
+import initialState from "./initial";
+
+// Reducer functions
+const player1 = state => ({ ...state, player1: state.player1 + 1});
+const player2 = state => ({ ...state, player2: state.player2 + 1});
+
+const server = state => {
+  const { servingP1, player1, player2 } = state;
+  let modulo = player1 >= 20 && player2 >= 20 ? 2 : 5;
+  let serveChange = (player1 + player2) % modulo === 0;
+
+  return {
+    ...state,
+    servingP1: serveChange ? !servingP1 : servingP1,
+  }
+};
+
+const winner = state => {
+  const { winner, player1, player2 } = state;
+
+  if (winner > 0 ) {
+    return { ...state };
+  } 
+  else if ( player1 >= 21 && player1 - player2 > 1 ) {
+    return { ...state, winner: 1 };
+  } 
+  else if ( player2 >= 21 && player2 - player1 > 1 ) {
+    return { ...state, winner: 2 };
+  } else {
+    return { ...state };
+  }
+}
+
+const history = state => {
+  if(state.winner === 0) {
+    return state;
+  }
+
+  const { player1, player2, winner } = state;
+
+  return {
+    ...state,
+    gameHistory: [ 
+      ...state.gameHistory,
+      {
+        player_1: {
+          score: player1,
+          won: winner === 1
+        },
+        player2: {
+          score: player2,
+          won: winner === 2
+        },
+      }
+    ]
+  } 
+}
+
+// Reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PLAYER_1": return history(winner(server(player1(state))));
+    case "PLAYER_2": return history(winner(server(player2(state))));
+    case "RESET": return { ...initialState, gameHistory: state.gameHistory };
+    default : return state;
+  }
+};
+
+export default reducer;
