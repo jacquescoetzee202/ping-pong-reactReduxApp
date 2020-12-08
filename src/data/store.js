38a4fadb1746c90e90db154a02e6f0b4fe46b6f9
Jacques <jacquescoetzee91@gmail.com>
2020-12-08 10:22:45 +0000

    added persistance to app with redux-localstorage, keeping state on refresh

--- src/data/store.js
+++ src/data/store.js
@@ -1,12 +1,15 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
+import persisState from "redux-localstorage";
 
 import initialState from "./initial";
 import reducer from "./reducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),  
+  composeEnhancers(persisState())  
 );
 
 export default store;
