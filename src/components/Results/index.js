0cc55d880cb0e0d68031e81d7801b774dace0724
Jacques <jacquescoetzee91@gmail.com>
2020-12-10 10:45:59 +0000

    updated results table to have translated props

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,13 +1,21 @@
 import { connect } from "react-redux";
 import ResultsTable from "./ResultsTable";
+import translate from "../../language/translate";
 
 const mapStateToProps = state => {
+    
+    const headings = ["match","winner","looser","score"];
+    let translated = headings.map( heading => translate(state.language, heading));
+    
+
     return {
         results: state.gameHistory,
-        player1Text: "Jacques",
-        player2Text: "LOOSER",
-        headings: ["Game","Winner","Looser","Score"],
+        player1Text: state.player1Name,
+        player2Text: state.player2Name,
+        headings: translated,
     };
 };
+
+
 
 export default connect(mapStateToProps)(ResultsTable);
